811dfec32661f2650faa3a5735e392d8b6a05541
Harari2008 <vrrh2008@gmail.com>
2025-06-24 16:09:07 +0300

    Update GameResult.js

--- Entities/GameResult.js
+++ Entities/GameResult.js
@@ -1,4 +1,37 @@
-// Sample code for demonstration
-export default function Component() {
-  return <div>Component Placeholder</div>;
+{
+  "name": "GameResult",
+  "type": "object",
+  "properties": {
+    "game_id": {
+      "type": "string",
+      "description": "מזהה המשחק"
+    },
+    "player_email": {
+      "type": "string",
+      "description": "מייל השחקן"
+    },
+    "buy_in": {
+      "type": "number",
+      "description": "סכום הקנייה"
+    },
+    "final_amount": {
+      "type": "number",
+      "description": "סכום סופי שהשחקן סיים איתו"
+    },
+    "profit_loss": {
+      "type": "number",
+      "description": "רווח או הפסד (חיובי = רווח, שלילי = הפסד)"
+    },
+    "confirmed_by_host": {
+      "type": "boolean",
+      "default": false,
+      "description": "האם התוצאה אושרה על ידי מארח המשחק"
+    }
+  },
+  "required": [
+    "game_id",
+    "player_email",
+    "buy_in",
+    "final_amount"
+  ]
 }
